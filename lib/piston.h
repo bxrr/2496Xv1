b926c2cc68c05073805810a0bf68b0456e4f1e6c
YKawesome <72176181+YKawesome@users.noreply.github.com>
2022-09-08 15:07:47 -0700

    fixed piston

--- lib/piston.h
+++ lib/piston.h
@@ -10,17 +10,31 @@ private:
     pros::ADIDigitalOut piston;
     bool status;
     int num_actuations;
+    bool invert;
 
 public:
-    Piston(int port, bool init_status=false) 
-    : piston(port) {
+    Piston(int port, bool init_status=false, bool invert=false) 
+    : piston(port) 
+    {
         set(init_status);
     }
 
+
     void set(bool extend)
     {
-        piston.set_value(extend);
-        status = extend;
+        if(invert) 
+        {
+            piston.set_value(!extend);
+            status = !extend;
+        }
+        
+        else 
+        {
+            piston.set_value(extend);
+            status = extend;
+        }
+
+        
     } 
     
 
